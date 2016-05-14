b1ca1e0c03c21ac33e3ad7c909a14761b82cbeea
Rob Debbane <studio@robertdebbane.com>
2016-05-14 10:43:16 -0400

    basic framework of app

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = new express();
 var path = require('path');
 
+app.use(express.static(path.join(__dirname, '../browser')));
+app.use(express.static(path.join(__dirname, '../node_modules')));
 app.use('/', require(path.join(__dirname, './routes')));
 
 var port = 8080 || process.env.PORT;
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,10 @@ var router = require('express').Router();
 var path = require('path');
 var colorTable = require(path.join( __dirname, '../db/color.table')).colorTable;
 
+router.get('/', function (req, res, next) {
+  res.sendFile('index.html');
+});
+
 router.get('/colors', function (req, res, next) {
   res.json(colorTable);
   next();
