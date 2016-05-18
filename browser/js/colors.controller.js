89bfe23e42295d9e3cb8918cc6f865117a2e2d0c
Rob Debbane <studio@robertdebbane.com>
2016-05-18 09:35:49 -0400

    make colors clickable - not quite working

--- browser/js/colors.controller.js
+++ browser/js/colors.controller.js
@@ -7,7 +7,7 @@ app.controller('ColorTableCtrl', function($scope, ColorTableFactory, colorTable)
       return $scope.currentColor;
     };
 
-    function _setColor(newColor) {
+    $scope.setColor = function (newColor) {
       console.log('newColor: ', newColor);
       $scope.currentColor = {
         hue: _colorTable[newColor.colIdx][0],
@@ -15,40 +15,40 @@ app.controller('ColorTableCtrl', function($scope, ColorTableFactory, colorTable)
       };
       $scope.currentPalette = _colorTable[newColor.colIdx].slice().reverse();
 
-    }
+    };
 
-    _setColor( _colorTable[0][5][3] );
+    $scope.setColor( _colorTable[0][5][3] );
 
   $scope.moreHue = function(targetHue) {
     var newColor = ColorTableFactory.moreHue($scope.currentColor.values, targetHue);
-    _setColor(newColor);
+    $scope.setColor(newColor);
     console.log('currentPalette', $scope.currentPalette);
   };
 
   $scope.findColor = function(targetHue) {
     var newColor = ColorTableFactory.findColor(targetHue);
-    _setColor(newColor);
+    $scope.setColor(newColor);
   };
 
   $scope.moreContrast = function() {
     var newColor = ColorTableFactory.moreContrast($scope.currentColor.values);
-    _setColor(newColor);
+    $scope.setColor(newColor);
   };
 
   $scope.lessContrast = function() {
     var newColor = ColorTableFactory.lessContrast($scope.currentColor.values);
-    _setColor(newColor);
+    $scope.setColor(newColor);
   };
 
   $scope.lighter = function() {
     console.log('lighter triggered');
     var newColor = ColorTableFactory.lighter($scope.currentColor.values);
-    _setColor(newColor);
+    $scope.setColor(newColor);
   };
 
   $scope.darker = function() {
     var newColor = ColorTableFactory.darker($scope.currentColor.values);
-    _setColor(newColor);
+    $scope.setColor(newColor);
   };
 
   $scope.save = function() {
@@ -70,56 +70,56 @@ app.controller('ColorTableCtrl', function($scope, ColorTableFactory, colorTable)
 
   var colorNames = ['red', 'blue', 'green', 'violet', 'orange', 'yellow'];
 
- //annyang.addCommands(commands);
-
-  annyang.addCallback('result', function(userSaid) {
-    //console.log(userSaid);
-    var words = [];
-    userSaid.forEach(function(phrase) {
-      console.log(phrase);
-      if (phrase.indexOf('lighter') !== -1) {
-        $scope.lighter();
-      }
-      else if (phrase.indexOf('darker') !== -1) $scope.darker();
-      else if (phrase.indexOf('perfect') !== -1) $scope.save();
-      else if (phrase.indexOf('more contrast') !== -1) $scope.moreContrast();
-      else if (phrase.indexOf('less contrast') !== -1) $scope.lessContrast();
-      else if (phrase.indexOf('find') !== -1) {
-        words = phrase.split(' ');
-          words.forEach(function(word) {
-            if (colorNames.indexOf(word.toLowerCase()) !== -1) {
-              $scope.findColor(word.toLowerCase());
-            }
-          });
-      }
-      else {
-        words = phrase.split(' ');
-          words.forEach(function(word) {
-            if (colorNames.indexOf(word.toLowerCase()) !== -1) {
-              $scope.moreHue(word.toLowerCase());
-            }
-          });
-        }
-
-
-      $scope.$digest();
-    });
-  });
-
-      // var words = phrase.split(' ');
-      // if (words.indexOf('more') !== -1) {
-      //   words.forEach(function(word) {
-      //     if (colorNames
-      //   })
-      //}
-  // });
-
-  annyang.addCallback('resultMatch', function(userSaid, commandText, phrases) {
-    console.log(userSaid); // sample output: 'hello'
-    console.log(commandText); // sample output: 'hello (there)'
-    console.log(phrases); // sample output: ['hello', 'halo', 'yellow', 'polo', 'hello kitty']
-  });
-
-  annyang.start({autoRestart: true});
+ // //annyang.addCommands(commands);
+ //
+ //  annyang.addCallback('result', function(userSaid) {
+ //    //console.log(userSaid);
+ //    var words = [];
+ //    userSaid.forEach(function(phrase) {
+ //      console.log(phrase);
+ //      if (phrase.indexOf('lighter') !== -1) {
+ //        $scope.lighter();
+ //      }
+ //      else if (phrase.indexOf('darker') !== -1) $scope.darker();
+ //      else if (phrase.indexOf('perfect') !== -1) $scope.save();
+ //      else if (phrase.indexOf('more contrast') !== -1) $scope.moreContrast();
+ //      else if (phrase.indexOf('less contrast') !== -1) $scope.lessContrast();
+ //      else if (phrase.indexOf('find') !== -1) {
+ //        words = phrase.split(' ');
+ //          words.forEach(function(word) {
+ //            if (colorNames.indexOf(word.toLowerCase()) !== -1) {
+ //              $scope.findColor(word.toLowerCase());
+ //            }
+ //          });
+ //      }
+ //      else {
+ //        words = phrase.split(' ');
+ //          words.forEach(function(word) {
+ //            if (colorNames.indexOf(word.toLowerCase()) !== -1) {
+ //              $scope.moreHue(word.toLowerCase());
+ //            }
+ //          });
+ //        }
+ //
+ //
+ //      $scope.$digest();
+ //    });
+ //  });
+ //
+ //      // var words = phrase.split(' ');
+ //      // if (words.indexOf('more') !== -1) {
+ //      //   words.forEach(function(word) {
+ //      //     if (colorNames
+ //      //   })
+ //      //}
+ //  // });
+ //
+ //  annyang.addCallback('resultMatch', function(userSaid, commandText, phrases) {
+ //    console.log(userSaid); // sample output: 'hello'
+ //    console.log(commandText); // sample output: 'hello (there)'
+ //    console.log(phrases); // sample output: ['hello', 'halo', 'yellow', 'polo', 'hello kitty']
+ //  });
+ //
+ //  annyang.start({autoRestart: true});
 
 });
